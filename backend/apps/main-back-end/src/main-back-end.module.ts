a79279876f8bfc7019bf33dc3363032e97a34eaf
ronanter <ronan1terras@gmail.com>
2025-01-14 19:23:25 +0200

    [ADD] schemas folder

--- backend/apps/main-back-end/src/main-back-end.module.ts
+++ backend/apps/main-back-end/src/main-back-end.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from '@app/common';
 import { MainBackEndController } from './main-back-end.controller';
 import { MainBackEndService } from './main-back-end.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: '/.apps/orders/.env',
+    }),
+    DatabaseModule,
+  ],
   controllers: [MainBackEndController],
   providers: [MainBackEndService],
 })
